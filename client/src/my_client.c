d7459abc3ce8044cb1004a630533704677a5efbe
Gustave Delecroix <gustave.delecroix@epitech.eu>
2024-04-01 01:45:06 +0200

    feat(client): receive the connection message and send any other

--- client/src/my_client.c
+++ client/src/my_client.c
@@ -13,18 +13,23 @@
 #include "my_string.h"
 #include "macro.h"
 #include "client.h"
+#include "my_socket.h"
 
 int client_loop(struct client_in *client)
 {
     bool connected = true;
-    char *buffer = NULL;
+    char *buffer_send = NULL;
     size_t n = 0;
+    char buffer_recv[SIZE_INCOME_MESS] = {0};
 
+    recv(client->socket, buffer_recv, SIZE_INCOME_MESS, 0);
+    printf("%s\n", buffer_recv);
     while (connected) {
-        if (getline(&buffer, &n, stdin) < 0) {
+        if (getline(&buffer_send, &n, stdin) < 0) {
             connected = false;
         }
-        send(client->socket, buffer, my_strlen(buffer), 0);
+        printf("buffer send >%s\n", buffer_send);
+        send(client->socket, buffer_send, my_strlen(buffer_send), 0);
     }
     return EXIT_SUCCESS;
 }

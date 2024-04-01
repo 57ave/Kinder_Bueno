76ea54d1d2d9835062ca8c899c58e4ee13676543
Gustave Delecroix <gustave.delecroix@epitech.eu>
2024-04-02 00:40:14 +0200

    feat(client): create a struct with method which specify methods for request

---
+++ client/src/send_request.c
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2023
+** my_network
+** File description:
+** client request handling
+*/
+
+#include "client.h"
+#include "my_string.h"
+#include "macro.h"
+
+int send_request(struct client_in *client, char *buffer)
+{
+    int exit_status = 0;
+
+    for (int i = 0; REQ_TAB[i].method != -1; i++) {
+        if (my_strcmp(REQ_TAB[i].action, buffer)) {
+            exit_status = REQ_TAB[i].pf(client, REQ_TAB[i].method);
+        }
+    }
+    return EXIT_SUCCESS;
+}
--- include/client.h
+++ include/client.h
@@ -8,6 +8,8 @@
 #ifndef CLIENT_H
     #define CLIENT_H
     #include <netinet/in.h>
+    #include <stddef.h>
+    #include "my_socket.h"
 
 int my_client(void);
 
@@ -15,6 +17,37 @@ struct client_in {
     int socket;
     struct sockaddr_in addr;
     socklen_t addrlen;
+};
+
+typedef enum method {
+    CONNECT = 1,
+    GET,
+    POST,
+    PATCH,
+    DELETE,
+    LAST_METHOD
+} method_t;
+
+struct request_model {
+    method_t method;
+    char *action;
+    int (*pf)(struct client_in *, method_t);
+};
+
+struct request_data {
+    method_t method;
+    short int current_packet;
+    short int total_packet;
+    char buffer[BUFFER_MAX];
+};
+
+int handle_signin(struct client_in *client, method_t method);
+
+int send_request(struct client_in *client, char *buffer);
+
+static const struct request_model REQ_TAB[] = {
+    {POST, "SIGN-IN", &handle_signin},
+    {LAST_METHOD, NULL, NULL}
 };
 
 #endif /* !CLIENT_H_ */

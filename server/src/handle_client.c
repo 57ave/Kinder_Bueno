23970db422914ccb1710e3c8bd3b1279bb793c5f
Gustave Delecroix <gustave.delecroix@epitech.eu>
2024-04-07 18:08:28 +0200

    feat(server): call the function for already exist account

--- server/src/handle_client.c
+++ server/src/handle_client.c
@@ -18,22 +18,31 @@ int connect_method_handler(struct server_in *serv, char buffer[BUFFER_MAX], int 
     struct data_128 user_data = {0};
     size_t n_recv = recv(
         serv->clients[client_id].sock_fd, &user_data, sizeof(struct data_128), 0);
+    int return_value = 0;
 
     if (n_recv < 0) {
         perror("recv");
         return EXIT_FAIL;
     }
-    if (my_strcmp("SIGN-IN", buffer) == 0)
-        return register_account(serv->ht, serv->clients[client_id].sock_fd, &user_data);
-    if (my_strcmp("LOGIN", buffer) == 0)
-        return login_account(serv->ht, serv->clients[client_id].sock_fd, &user_data);
+    if (check_connected_account(serv, &user_data) == EXIT_SUCCESS)
+        return already_connected_account(serv->clients[client_id].sock_fd);
+    if (my_strcmp("SIGN-IN", buffer) == 0) {
+        return_value = register_account(serv->ht, serv->clients[client_id].sock_fd, &user_data);
+        if (return_value == EXIT_SUCCESS)
+            my_strcpy(serv->clients[client_id].username, user_data.key);
+        return return_value;
+    }
+    if (my_strcmp("LOGIN", buffer) == 0) {
+        return_value = login_account(serv->ht, serv->clients[client_id].sock_fd, &user_data);
+        if (return_value == EXIT_SUCCESS)
+            my_strcpy(serv->clients[client_id].username, user_data.key);
+        return return_value;
+    }
     return EXIT_FAIL;
-
 }
 
 static int find_method(struct server_in *serv, char buffer[BUFFER_MAX], int client_id)
 {
-    printf("code = %i, buffer after code %s\n", buffer[0], &buffer[1]);
     for (int i = 0; METHOD_HANDLE[i].method != LAST_METHOD; i++) {
         if (METHOD_HANDLE[i].method == *buffer) {
             return METHOD_HANDLE[i].handler(serv, &buffer[1], client_id);

c21282fb9ac33358b5c56559117b05b78b8431ea
Gustave Delecroix <gustave.delecroix@epitech.eu>
2024-04-07 18:11:21 +0200

    feat: add prototype for the already connected account handling

--- include/account.h
+++ include/account.h
@@ -15,6 +15,7 @@
 enum code_account {
     OK = 1,
     ALREADY_EXIST,
+    ALREADY_CONNECTED,
     NOT_FOUND,
     WRONG_PASSWORD,
     FAIL,
@@ -27,5 +28,9 @@ enum code_account connect_account(hashtable_t *ht, struct data_128 *data);
 hashtable_t *init_account_tab(void);
 
 int register_account(hashtable_t *ht, int client_socket, struct data_128 *user_data);
+
+int already_connected_account(int client_socket);
+
+int check_connected_account(struct server_in *serv, struct data_128 *data);
 
 int login_account(hashtable_t *ht, int client_socket, struct data_128 *user_data);

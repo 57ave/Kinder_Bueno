8bab9aab32d08c3de37bd7d49cf633a0422eed99
Gustave Delecroix <gustave.delecroix@epitech.eu>
2024-04-07 18:07:12 +0200

    feat(server/account_handling): handle already exist account

---
+++ server/src/account_handling/already_exist_account.c
@@ -0,0 +1,38 @@
+/*
+** EPITECH PROJECT, 2024
+** server
+** File description:
+** register a user/client
+*/
+
+#include <stddef.h>
+#include <unistd.h>
+#include <stdio.h>
+#include "macro.h"
+#include "my_socket.h"
+#include "account.h"
+#include "my_string.h"
+
+int check_connected_account(struct server_in *serv, struct data_128 *data)
+{
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (my_strcmp(data->key, serv->clients[i].username) == 0) {
+            printf("user already connected\n");
+            return EXIT_SUCCESS;
+        }
+    }
+    return EXIT_FAIL;
+}
+
+int already_connected_account(int client_socket)
+{
+    char exit_code = ALREADY_CONNECTED;
+    size_t n = 0;
+
+    n = send(client_socket, &exit_code, exit_code, 0);
+    if (n < 0) {
+        perror("send");
+        return EXIT_FAIL;
+    }
+    return EXIT_SUCCESS;
+}

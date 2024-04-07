ec82f7c90f56ec0f7e610171e7659350ab83ff58
Gustave Delecroix <gustave.delecroix@epitech.eu>
2024-04-07 16:20:19 +0200

    update: new function prototype

---
+++ include/account.h
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2023
+** Kinder_bueno
+** File description:
+** account
+*/
+
+    #include "hashtable.h"
+    #include "my_unistd.h"
+    #include "my_socket.h"
+    #include "client.h"
+
+    #define LEN_TAB_ACCOUNT 100
+
+enum code_account {
+    OK = 1,
+    ALREADY_EXIST,
+    NOT_FOUND,
+    WRONG_PASSWORD,
+    FAIL,
+};
+
+enum code_account new_account(hashtable_t *ht, struct data_128 *data);
+
+enum code_account connect_account(hashtable_t *ht, struct data_128 *data);
+
+hashtable_t *init_account_tab(void);
+
+int register_account(hashtable_t *ht, int client_socket, struct data_128 *user_data);
+
+int login_account(hashtable_t *ht, int client_socket, struct data_128 *user_data);
